2775da0bfbdeb58b2f0f1da4b2bf818794b8407d
yuubd <gc.kim93@hotmail.com>
2019-01-26 18:30:56 -0800

    added more on login

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,55 +1,96 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 class Login extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			email: '',
+			password: ''
+		};
+		this.onEmailChange = this.onEmailChange.bind(this);
+		this.onPasswordChange = this.onPasswordChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	onEmailChange(e) {
+		this.setState({ email: e.target.value });
+	}
+
+	onPasswordChange(e) {
+		this.setState({ password: e.target.value });
+	}
+
+	handleSubmit() {
+		console.log('submit clicked!');
 	}
 
 	render() {
 		return (
-			<main className="pa4 black-80">
-				<form className="measure center">
-					<fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-						<legend className="f4 fw6 ph0 mh0">Login</legend>
-						<div className="mt3">
-							<label className="db fw6 lh-copy f6" for="email-address">
-								Email
-							</label>
-							<input
-								className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-								type="email"
-								name="email-address"
-								id="email-address"
-							/>
-						</div>
-						<div className="mv3">
-							<label className="db fw6 lh-copy f6" for="password">
-								Password
-							</label>
-							<input
-								className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-								type="password"
-								name="password"
-								id="password"
-							/>
-						</div>
-					</fieldset>
-					<div className="">
-						<input
-							className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-							type="submit"
-							value="Log in"
-						/>
-					</div>
-					<div className="lh-copy mt3">
-						<a href="#0" className="f6 link dim black db">
+			<div>
+				<nav className=" dt h-10 w-100 relative bg-black-80 center">
+					<div className="dtc v-mid tr pa3">
+						<Link
+							className="f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3"
+							to="/"
+						>
+							How it Works
+						</Link>
+						<Link
+							className="f6 fw4 hover-white no-underline white-70 dib ml2 pv2 ph3 ba"
+							to="/register"
+						>
 							Register
-						</a>
-						<a href="#0" className="f6 link dim black db">
-							Forgot your password?
-						</a>
+						</Link>
 					</div>
-				</form>
-			</main>
+				</nav>
+				<main className="pa4 black-80">
+					<form className="measure center">
+						<fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+							<legend className="f4 fw6 ph0 mh0">Login</legend>
+							<div className="mt3">
+								<label className="db fw6 lh-copy f6" htmlFor="email-address">
+									Email
+								</label>
+								<input
+									onChange={this.onEmailChange}
+									className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+									type="email"
+									name="email-address"
+									id="email-address"
+								/>
+							</div>
+							<div className="mv3">
+								<label className="db fw6 lh-copy f6" htmlFor="password">
+									Password
+								</label>
+								<input
+									onChange={this.onPasswordChange}
+									className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+									type="password"
+									name="password"
+									id="password"
+								/>
+							</div>
+						</fieldset>
+						<div className="">
+							<input
+								onClick={this.handleSubmit}
+								className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+								type="submit"
+								value="Log in"
+							/>
+						</div>
+						<div className="lh-copy mt3">
+							<Link to="/register" className="f6 link dim black db">
+								Register
+							</Link>
+							<Link to="/" className="f6 link dim black db">
+								Forgot your password?
+							</Link>
+						</div>
+					</form>
+				</main>
+			</div>
 		);
 	}
 }
