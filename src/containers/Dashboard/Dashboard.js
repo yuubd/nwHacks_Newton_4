81b952fe9475367387edac0125394e56437af65a
John Sastrillo <john.sastrillo@gmail.com>
2019-01-26 22:10:41 -0800

    moved dashboard to navbar

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import * as d3 from 'd3';
 class Dashboard extends Component {
 	constructor(props)
 	{
@@ -10,6 +11,7 @@ class Dashboard extends Component {
 		return (
 			<div>
 			<nav className=" dt h-10 w-100 relative bg-black-80 center">
+			<div className="f6 fw4 hover-white no-underline white-70 dib ml2 pv2"><h1> Dashboard Page </h1></div>
 			<div className="dtc v-mid tr pa3">
 				<Link
 					className="f6 fw4 hover-white no-underline white-70 dib ml2 pv2 ph3 ba"
@@ -25,9 +27,6 @@ class Dashboard extends Component {
 				</Link>
 			</div>
 			</nav>
-			<div>
-				<h1>DASHBOARD PAGE</h1>
-			</div>
 			</div>
 		);
 	}
